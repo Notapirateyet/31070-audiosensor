8f57f8627055bc23359d074d92daa673bd32cc52
AsgerJK <s163933@student.dtu.dk>
2021-06-17 14:47:47 +0200

    New comments

--- SoundLevelMeter/src/microphonePlayground.cpp
+++ SoundLevelMeter/src/microphonePlayground.cpp
@@ -14,23 +14,29 @@
 const int micPin = A0;
 const int potPin = A2;
 int old_pot_reading = 0;
+const int MINIMAL_MEASUREMENTS_FOR_DATA_PROCESSING = 200;
 
 //Variables
 CircularBuffer<int, BUFFER_SIZE> mic_readings;
+CircularBuffer<int, 5> five_max_values;
 volatile bool buffer_overflow_flag = false;
+volatile bool using_ISP_variable_flag = false;
 
 // Timing
-bool measure_dataprocessing = true;
+bool measure_dataprocessing = false;
 unsigned long data_average_fs, data_max_fs, data_min_fs, data_last_sample_time;
 
 void measureInterrupt()
 {
-    // Read the analog value, Range: 0 - 65520
-    int micVal = analogRead(micPin);
-    // Store the value, and check if the buffer is full
-    if (!mic_readings.push(micVal))
+    if (!using_ISP_variable_flag) // Check if the buffer is in use
     {
-        buffer_overflow_flag = true; // Flag that the buffer is full
+        // Read the analog value, Range: 0 - 65520
+        int micVal = analogRead(micPin);
+        // Store the value, and check if the buffer is full
+        if (!mic_readings.push(micVal))
+        {
+            buffer_overflow_flag = true; // Flag that the buffer is full
+        }
     }
 }
 
@@ -40,7 +46,8 @@ void setupMicrophone()
     analogReadResolution(16); //resolution is set as high as possible
     earMeter.set_max_value(40020);
     earMeter.set_min_value(0);
-    MyTimer5.begin(200); 
+    // Initialize timer
+    MyTimer5.begin(800); // [Hz]
     MyTimer5.attachInterrupt(measureInterrupt);
     MyTimer5.start();
     // Reset timing variables
@@ -53,28 +60,37 @@ void setupMicrophone()
     }
 }
 
+// Read the collected data and send info
 void loopMicrophone()
 {
-    // Read the collected data and send info the serial prompt
+    // Loop variables
     int sound_level_raw = 0;
     int measurements = mic_readings.size();
     int current_reading;
     int max = 0;
-    int min = 10000; // Large
+    int min = INT16_MAX; // Large
     int pot_reading;
-    // Safety measure if the buffer is empty (unlikely)
-    if (measurements < 200) // Currently test mode, set == 0 for real
-    {
-        //measureInterrupt(); // Remove after debugging check
-        return;
-    }
+    float deviations[5];
+    float average;
 
-    //Serial.println("Starting data processing");
+    //
+    // Check entry conditions
+    //
+    if (measurements < MINIMAL_MEASUREMENTS_FOR_DATA_PROCESSING) // Could be changed.
+    {
+        return; // Bad style to return in the middle of a function.
+    }
+    // else -> rest of this function
+
+    // Change the sampling flag
+    using_ISP_variable_flag = true;
+
+    //
     // Change output threshold
+    //
     analogReadResolution(4); // No need for big fancy precise samples
     pot_reading = analogRead(potPin);
-    //    if (old_pot_reading - pot_reading >= 0x0010 || old_pot_reading - pot_reading <= -0x0010)
-    if (pot_reading != old_pot_reading)
+    if (pot_reading != old_pot_reading) // Only update on a change
     {
         // Update vars
         old_pot_reading = pot_reading;
@@ -87,7 +103,15 @@ void loopMicrophone()
 
     analogReadResolution(16); // Back to old resolution
 
+    //
     // Do some data processing
+    //
+    for (int i = 0; i < 5; i++)
+    {
+        five_max_values.push(0);
+    }
+
+    // Safety measure if the buffer is empty
     while (!mic_readings.isEmpty())
     {
         current_reading = mic_readings.pop();
@@ -100,17 +124,35 @@ void loopMicrophone()
         {
             min = current_reading;
         }
+        for (int i = 0; i < 5; i++)
+        {
+            if (five_max_values[i] < current_reading)
+            {
+                five_max_values.push(current_reading);
+            }
+        }
     }
-    float average = (float)sound_level_raw / (float)measurements;
+    average = (float)sound_level_raw / (float)measurements;
+    for (int i = 0; i < 5; i++)
+    {
+        deviations[i] = five_max_values.pop() - average;
+    }
 
+    // Timing code part 1, currently measureing LCD time
     if (measure_dataprocessing == true)
     {
         data_last_sample_time = micros(); // Update timing variable
     }
 
+    //
+    // Output, lcd, led, serial, cloud
+    //
     lcd.setCursor(0, 0);
     lcd.print(average);
+    lcd.write(byte(0));
+    lcd.write(byte(1));
 
+    // Timing code part 2
     if (measure_dataprocessing == true)
     {
         // Timing measurements, measures the time since the last update of data_last_sample_time
@@ -133,7 +175,10 @@ void loopMicrophone()
         Serial.println(data_average_fs);
     }
 
+    // LED output
     earMeter.write_value(average);
+
+    // Serial output
     /*
     Serial.print("Measurements: ");
     Serial.print(measurements);
@@ -146,8 +191,24 @@ void loopMicrophone()
     Serial.print("; Potmeter: ");
     Serial.print(pot_reading);
     Serial.print("; Max LED: ");
-    Serial.println(earMeter.get_max_value());
-
-    write_dB_read(average);
+    Serial.print(earMeter.get_max_value());
     */
+    Serial.print("; max_deviation: ");
+    Serial.print(max - average);
+    Serial.print("; 5 deviations: ");
+    Serial.print(deviations[0]);
+    Serial.print(" | ");
+    Serial.print(deviations[1]);
+    Serial.print(" | ");
+    Serial.print(deviations[2]);
+    Serial.print(" | ");
+    Serial.print(deviations[3]);
+    Serial.print(" | ");
+    Serial.println(deviations[4]);
+
+    // Cloud output
+    write_dB_read(average);
+
+    // Set the sampling flag back
+    using_ISP_variable_flag = false;
 }
